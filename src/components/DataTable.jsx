eb2589a341a776e2e3acce544f8e1dd12a523dd0
AbyssRat <yunothefurry@gmail.com>
2025-12-11 14:13:41 +0100

    ja

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -14,6 +14,20 @@ const DataTable = ({ adatok }) => {
                         <th>belepes</th>
                     </tr>
                 </thead>
+
+                <tbody>
+                    {adatok.map((adatsor) => (
+                        <tr key = {adatsor.id}>
+                            <td>{adatsor.id}</td>
+                            <td>{adatsor.TeljesNev}</td>
+                            <td>{adatsor.Eletkor}</td>
+                            <td>{new Date(adatsor.BelepesDatuma).toLocaleDateString()}
+                            </td>
+                            
+                        </tr>
+
+                    ))}
+                </tbody>
             </table>
         </div>
     )
