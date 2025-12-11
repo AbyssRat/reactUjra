b190910380d02c6bb325430ccb792c1518f1f8c6
AbyssRat <yunothefurry@gmail.com>
2025-12-11 13:09:59 +0100

    aha

---
+++ src/components/KeresoPanel.jsx
@@ -0,0 +1,9 @@
+const KeresoPanel = () => {
+    return(
+        <div>
+            <h2>kereso panel comp</h2>
+        </div>
+    )
+}
+
+export default KeresoPanel;
---
+++ src/components/LoadData.jsx
@@ -0,0 +1,9 @@
+const LoadData = () => {
+    return (
+        <div>
+            <h2>load complete</h2>
+        </div>
+    )
+}
+
+export default LoadData;
